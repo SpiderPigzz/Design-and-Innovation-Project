290c12b81ab5cc41544be07f6da3b24ce8a11bcf
SpiderPigzz <Ricky.winarko@gmail.com>
2022-08-31 03:12:50 +0800

    Added menu drawer and started using react native paper

--- KungFoodHippo/App.js
+++ KungFoodHippo/App.js
@@ -1,132 +1,98 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import * as Font from 'expo-font';
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { useState } from 'react';
+import { DrawerActions, createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { MD3LightTheme as DefaultTheme, Provider as PaperProvider, Text, Appbar, Snackbar, BottomNavigation, Button, Card, Surface, Title, Paragraph, Drawer } from 'react-native-paper';
+import { styles } from './Styles.js'
+import { HippoCard } from './Components/TestCard.js';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
 
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fontsLoaded: false
-    }
-  }
 
-  async loadFonts() {
-    await Font.loadAsync({
-      'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    });
-    this.setState({ fontsLoaded: true });
-  }
+export default function KungFoodHippo() {
+  const [active, setActive] = React.useState('');
+  
 
-  componentDidMount() {
-    this.loadFonts();
-  }
+  
 
-  render() {
-    if (this.state.fontsLoaded) {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-          <Pressable style={styles.button} android_ripple={{ color: 'white', borderless: false }} onPress={this.onClickHandler}>
-            <Text style={styles.buttontext}>TEST</Text>
-          </Pressable>
-          <Card containerStyle={styles.card} image={require('./assets/favicon.png')}>
-            <Text style={styles.paragraph}>
-              React Native Card View for Android and IOS using
-              "react-native-elements"
-            </Text>
-          </Card>
+  return (
 
-          <Card containerStyle={styles.cardSec}>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Image source={require('./assets/KFH.png')} backgroundColor={styles.primColor} resizeMode='contain'></Card.Image>
-            <Text style={{ marginBottom: 10 }}>
-              The idea with React Native Elements is more about component structure than actual design.
-            </Text>
-            <Pressable style={styles.button} android_ripple={{ color: 'white', borderless: false }} onPress={this.onClickHandler}>
-              <Text style={styles.buttontext}>TEST</Text>
-            </Pressable>
-          </Card>
-        </View>
-      );
-
-    }
-  }
-
-  onClickHandler = () => {
-
-  }
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Menu.Navigator initialRouteName="Home">
+          <Menu.Screen name="Home" component={HomeScreen} />
+          <Menu.Screen name="Notifications" component={NotificationsScreen} />
+        </Menu.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
 }
 
+function HomeScreen({ navigation }) {
+  const [buttonText, setButtonText] = useState('Click');
+  function handleClick() {
+    setButtonText('New text');
+  }
+
+  return (
+    <PaperProvider theme={theme}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => { }} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="calendar" onPress={() => { }} />
+        <Appbar.Action icon="magnify" onPress={() => { }} />
+      </Appbar.Header>
 
 
-styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      <SafeAreaView style={styles.container}>
+
+
+        <Text style={styles.text}>{buttonText}</Text>
+        <Button style={styles.button} android_ripple={{ color: 'white', borderless: false }} onPress={() => handleClick()}>
+          <Text style={styles.buttontext}>TEST</Text>
+        </Button>
+
+        <Card flex={1} style={styles.card}>
+          <Card.Content>
+            <Paragraph style={styles.paragraph}>
+              React Native Card View for Android and IOS using
+              "react-native-elements"
+            </Paragraph>
+          </Card.Content>
+        </Card>
+        <HippoCard flex={10}></HippoCard>
+      </SafeAreaView>
+    </PaperProvider>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Menu = createDrawerNavigator();
+
+
+const theme = {
+  DefaultTheme,
+  colors: {
+    primary: styles.primColor,
+    secondary: styles.secColor,
   },
+};
 
-  text: {
-    marginTop: 16,
-    paddingVertical: 8,
-    color: "#E76766",
-    textAlign: "center",
-    fontSize: 30,
-    fontFamily: "Roboto-Regular",
-    fontWeight: "bold"
-  },
-
-  buttontext: {
-    color: "#FFFFFF",
-    textAlign: "center",
-    fontSize: 20,
-    fontFamily: "Roboto-Regular",
-    fontWeight: "bold"
-  },
-
-  button: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderWidth: 0,
-    backgroundColor: "#E76766",
-    fontFamily: "Roboto-Regular",
-    borderRadius: 5,
-  },
-
-  buttonSec: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderWidth: 0,
-    backgroundColor: "#FFFFFF",
-    fontFamily: "Roboto-Regular",
-    borderRadius: 5,
-  },
-
-  card: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    elevation: 5,
-    borderWidth: 0,
-    backgroundColor: "#E76766",
-    borderRadius: 5
-  },
-
-  cardSec: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    elevation: 5,
-    borderWidth: 0,
-    backgroundColor: "#FFFFFF",
-    borderRadius: 5
-  },
-
-  primColor: "#E76766",
-
-  secColor: "#E76766"
-
-});
 
---
+++ KungFoodHippo/Styles.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 16,
+        justifyContent: 'center',
+    },
+
+    text: {
+        marginTop: 8,
+        paddingVertical: 8,
+        color: "#E76766",
+        textAlign: "center",
+        fontSize: 30,
+        fontFamily: "Roboto-Regular",
+        fontWeight: "bold"
+    },
+
+    buttontext: {
+        color: "#FFFFFF",
+        textAlign: "center",
+        fontSize: 20,
+        fontFamily: "Roboto-Regular",
+        fontWeight: "bold"
+    },
+
+    button: {
+        marginTop: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderWidth: 0,
+        backgroundColor: "#E76766",
+        fontFamily: "Roboto-Regular",
+        borderRadius: 5,
+    },
+
+    buttonSec: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderWidth: 0,
+        backgroundColor: "#FFFFFF",
+        fontFamily: "Roboto-Regular",
+        borderRadius: 5,
+    },
+
+    card: {
+        marginTop: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 3,
+        borderWidth: 0,
+        backgroundColor: "#E76766",
+        borderRadius: 5,
+    },
+
+    cardSec: {
+        marginTop: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 3,
+        borderWidth: 0,
+        backgroundColor: "#FFFFFF",
+        borderRadius: 5
+    },
+
+    primColor: "#E76766",
+
+    secColor: "#E76766"
+
+});
+
